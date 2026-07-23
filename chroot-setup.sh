44629e95af93d8cd055bbbe2d6977633bd1bc8dd
fuzifuziii <mops.sops123@gmail.com>
2026-07-23 18:03:59 +0300

    fix wheel

--- chroot-setup.sh
+++ chroot-setup.sh
@@ -29,7 +29,7 @@ read -rp "Create a new user? [Y/n]: " CREATE_USER
 CREATE_USER="${CREATE_USER:-Y}"
 if [[ "$CREATE_USER" =~ ^[Yy]$ ]]; then
   read -rp "Username: " NEWUSER
-  useradd -mG wheel "$NEWUSER"
+  useradd -a -G wheel "$NEWUSER"
   passwd "$NEWUSER"
   echo "%wheel ALL=(ALL:ALL) ALL" >/etc/sudoers
   chmod 440 /etc/sudoers
